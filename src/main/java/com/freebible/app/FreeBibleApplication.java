231999a8c5480fa28bd954702728925b09e64c2f
Oscar Sierra <oscarsierracontreras@gmail.com>
2023-04-11 06:43:30 -0500

    first commit to project

---
+++ src/main/java/com/freebible/app/FreeBibleApplication.java
@@ -0,0 +1,13 @@
+package com.freebible.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeBibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeBibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freebible/app/FreeBibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freebible.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeBibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
